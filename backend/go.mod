ed84d817327f2fab0b91ae669ad022a6c2c8c539
Arkan Damar <arkan.damar7@gmail.com>
2023-12-21 21:18:55 +0700

    project initialization

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/arkandamar/Arkanum/backend-arkanum
+
+go 1.21.0
---
+++ frontend-arkanum/src/App.tsx
@@ -0,0 +1,6 @@
+import Navbar from "./components/Navbar.js";
+import "./App.css";
+
+export default function App() {
+  return <Navbar />;
+}
